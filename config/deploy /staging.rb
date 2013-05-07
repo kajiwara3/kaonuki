a90aed5421b306d17414d9a2cf2d7aa2ff69cf3f
kajiwara3 <kajiwara3@gmail.com>
2013-05-08 08:07:35 +0900

    add deploy setting for staging

---
+++ config/deploy /staging.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+# RVM
+#### rvmのパス
+set :rvm_path, '~/.rvm'
+set :rvm_bin_path, "#{rvm_path}/bin"
+set :rvm_lib_path, "#{rvm_path}/lib"
+
+#SSHユーザーの設定
+set :user, 'ec2-user'
+
+# デプロイ先のディレクトリ設定
+set :deploy_to, "/home/#{user}"
+# デプロイ環境名
+set :rails_env, "staging"
+server "staging.fbb", :app, :web, :db, primary: true
+
+# Bundle
+set :bundle_flags, ""
+ssh_options[:keys] = %w(~/.ssh/aws/dev-aws.pem)
+
+namespace :deploy do
+end
