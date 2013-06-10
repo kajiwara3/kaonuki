d922f4d235c7e975730138ffe5c5d02af3af8823
kajiwara3 <kajiwara3@gmail.com>
2013-06-10 17:26:29 +0900

    デバッグログを削除した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
 #  before_filter :authenticate_user!
   def index
-    logger.info "==========================="
-    logger.info request.session_options[:id]
-    logger.info "==========================="
   end
 end
